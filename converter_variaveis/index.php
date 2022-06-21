87c44e1ba0b7e5a51975f6e0a4916b7628e30878
Alisson Miranda <alissonmichelmiranda@gmail.com>
2022-06-21 15:08:28 -0300

    curso PHP

---
+++ converter_variaveis/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$result = "2"; // é uma string
+
+/*
+$result = $result + 1; // é um interger 3
+$result = $result + 3.5; // é um double 6.5
+$result = 1 + 4.7; // é um double 5.7
+*/
+
+echo "Resultado exemplo um: " . $result . "<br>";
+var_dump($result);
+echo "<br>";
+echo "<br>";
+
+$result_dois = $result + 1;
+echo "Resultado exemplo dois: " . $result_dois . "<br>";
+var_dump($result_dois);
+echo "<br>";
+echo "<br>";
+
+
+
+
---
+++ sintaxe_basica_php/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $age = 24;
+    $firstName = "Fernanda Jessy";
+
+    echo "My name is " . $firstName . " and have " . $age . " years";
+
+    // Variáveis inicia com $ 
+    /*
+    tipos
+
+    inteiro = número sem casa decimal
+    ponto flutuante = número com casa decimal
+    String = caracteres/ textos
+    Objeto =  do tipo objeto a partir de uma classe
+    Array = conjunto de variáveis com chaves e valores
+    Booleanos = verdade ou falso
+
+    */
